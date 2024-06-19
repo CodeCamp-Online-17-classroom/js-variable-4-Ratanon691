ce169d57ecb3d82a48e0e72ca5a771d139e7511d
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-19 17:51:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// write code here
+const FirstName = `Rattanon`
+let LastName = `Tunwittaya`
+var NickName = `Non`
+console.log(FirstName, LastName, NickName)
+const FirstName = `Dik`
+let LastName = `Dik`
+var NickName = `Dukdik`
+birthDate = `1 Jan 1999`
